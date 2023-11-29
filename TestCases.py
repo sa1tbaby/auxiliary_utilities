eb98ce6220a4f68f468c50d8304445cb1a47ed23
sa1tbaby <sklitgames@gmail.com>
2023-11-29 22:03:05 +0300

    add TestCases.py

--- TestCases.py
+++ TestCases.py
@@ -1,29 +1,32 @@
-
+import sys
 def ExecutionTimer_test_case():
-
-    def test_func(a,b, **params):
-        result = 1
-
-        while result < 1000000000:
-            result += a + b
-
-        print(result, params)
 
     from Timers import ExecutionTimer
 
-    arg1 = 2
-    arg2 = 3
-    arg3 = {
-        '1': 1,
-        '2': 2,
-        '3': 3
-    }
+    with ExecutionTimer(SortingMethods_test_case) as f:
+        f.run()
 
-    with ExecutionTimer(test_func) as f:
-        f.run(arg1, arg2, **arg3)
+
+
+def SortingMethods_test_case():
+
+
+
+    from random import randint
+
+
+    test_list = [randint(0,100) for _ in range(80000)]
+
+    print(test_list)
+
+    print(test_list)
+
+
+
 
 if __name__ == '__main__':
     ExecutionTimer_test_case()
+
 
 
 
