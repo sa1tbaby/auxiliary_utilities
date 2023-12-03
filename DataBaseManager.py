d5efd7b1d4b47ea65dc39962f073cff1cc96e342
sa1tbaby <sklitgames@gmail.com>
2023-12-03 18:39:28 +0300

    add TestCases.py

---
+++ DataBaseManager.py
@@ -0,0 +1,15 @@
+
+class DBManger:
+
+    def __init__(self):
+        pass
+
+    def append(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def update(self):
+        pass
+
--- TestCases.py
+++ TestCases.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 def ExecutionTimer_test_case():
 
     from Timers import ExecutionTimer
@@ -10,10 +10,8 @@ def ExecutionTimer_test_case():
 
 def SortingMethods_test_case():
 
-
     from Sorters import SortingMethods
     from random import randint
-
 
     test_list = [randint(0,100) for _ in range(80000)]
 
@@ -21,22 +19,12 @@ def SortingMethods_test_case():
     test_list_tt = SortingMethods.bubble(test_list)
     print(test_list)
 
-def list_examples():
+def test_case():
 
-    from random import randint
-
-    list_ = [randint(0, 100) for _ in range(7)]
-    print(list_)
-    print(list_[int(len(list_)/2):])
-    print(list_[:int(len(list_) / 2)])
-
-    l1, l2 = list_[::int(len(list_)/2)]
-    print(l1,l2)
-
+    pass
 
 if __name__ == '__main__':
-    #ExecutionTimer_test_case()
-    list_examples()
+    test_case()
 
 
 
